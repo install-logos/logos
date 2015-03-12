f9ffef6878892c446f425b00eff5ca0827496abd
Ezra Umen <ezraumen@yahoo.com>
2015-03-12 15:12:33 -0500

    Added Vanilla Releng

---
+++ releng/airootfs/root/customize_airootfs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e -u
+
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+
+usermod -s /usr/bin/zsh root
+cp -aT /etc/skel/ /root/
+chmod 700 /root
+
+useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/zsh arch
+
+chmod 750 /etc/sudoers.d
+chmod 440 /etc/sudoers.d/g_wheel
+
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+
+systemctl enable pacman-init.service choose-mirror.service
+systemctl set-default multi-user.target
